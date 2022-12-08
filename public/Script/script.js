a49a9a8ddd12b67695104f934e7e73475eb80d36
Corey J Rutt <cjw.rutt@gmail.com>
2022-12-08 10:28:55 -0800

    Corey: Updates to personal page

--- public/Script/script.js
+++ public/Script/script.js
@@ -4,15 +4,15 @@ window.addEventListener("DOMContentLoaded", function() {
     Array.from(boxes, function(box) {
       box.addEventListener("click", function() {
         if (this.classList[2] == 'giovana') {
-            location.href = 'index.html';
+            location.href = 'giovana.html';
         } else if (this.classList[2] == 'corey') {
             location.href = 'corey.html';
         } else if (this.classList[2] == 'aaron') {
-            location.href = 'index.html';
+            location.href = 'aaron.html';
         } else if (this.classList[2] == 'mariela') {
-            location.href = 'index.html';
+            location.href = 'mariela.html';
         } else if (this.classList[2] == 'rose') {
-            location.href = 'index.html';
+            location.href = 'rose.html';
         }
       });
     });
